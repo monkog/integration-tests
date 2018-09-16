32a274e25bc6562d70848db0723c773abb4bd9cd
monkog <monkog@wp.pl>
2018-09-16 17:53:47 +0200

    integration tests - component and product

--- __tests__/server/component-tests.js
+++ __tests__/server/component-tests.js
@@ -1,4 +1,5 @@
 import { ProductsComponent } from "./../../front-end/scripts/components/products-component";
+import { Product } from "../../front-end/scripts/models/product";
 
 const jsdom = require('jsdom'); 
 const { JSDOM } = jsdom;
@@ -15,7 +16,29 @@ test("create div element for each product", () => {
         description: "123456789012345678901234567890",
         image: "img"
     };
-    new ProductsComponent(null, [p]);
+    let q = 
+    {
+        name: "123456789012345678901234567890",
+        id: 20,
+        description: "123456789012345678901234567890",
+        image: "img"
+    };
+    new ProductsComponent(document, [p, q]);
 
-    expect(document.children.length).toBe(1);
+    expect(document.firstChild.children.length).toBe(2);
+})
+
+test("name of product is rendered", () => {
+    let div = document.createElement("div");
+    let p = 
+    {
+        name: "123456789012345678901234567890",
+        id: 20,
+        description: "123456789012345678901234567890",
+        image: "img"
+    };
+    let cmp = new ProductsComponent(div, [new Product(p)]);
+    cmp.render();
+
+    expect(div.querySelector(".card-title").textContent).toBe("12345678901234567890");
 })
